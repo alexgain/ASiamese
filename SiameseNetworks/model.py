daaa239ef172e871509a891398f1241b2d22ba3c
Alex Gain <doctorjuice1@gmail.com>
2020-01-21 07:41:33 -0800

    added ASiamese model

--- SiameseNetworks/model.py
+++ SiameseNetworks/model.py
@@ -431,3 +431,108 @@ class ASiameseNetworks(Module):
         # L1 component-wise distance between vectors:
         x = torch.pow(torch.abs(x1 - x2), 2.0)
         return self.classifier(x, task)
+
+
+
+
+
+
+
+
+
+#############
+
+
+class ANet2(Module):
+    def __init__(self, input_shape):
+        """
+        :param input_shape: input image shape, (h, w, c)
+        """
+        super(ANet2, self).__init__()
+
+        self.features = Sequential(
+            AConv2d(input_shape[-1], 64, kernel_size=10),
+            ReLU(),
+            MaxPool2d(kernel_size=(2, 2), stride=2),
+
+            AConv2d(64, 128, kernel_size=7),
+            ReLU(),
+            MaxPool2d(kernel_size=(2, 2), stride=2),
+
+            AConv2d(128, 128, kernel_size=4),
+            ReLU(),
+            MaxPool2d(kernel_size=(2, 2), stride=2),
+
+            AConv2d(128, 256, kernel_size=4),
+            ReLU()
+        )
+        
+        def mod_forward(x, task, seq):
+            for y in seq:
+                try:
+                    x = y(x,task=task)
+                except:
+                    x = y(x)
+            return x                                                        
+        self.features.forward = mod_forward
+
+        # Compute number of input features for the last fully-connected layer
+        input_shape = (1,) + input_shape[::-1]
+        x = Variable(torch.rand(input_shape), requires_grad=False)
+        x = mod_forward(x, 0, self.features)
+        x = Flatten()(x)
+        n = x.size()[1]
+
+        self.classifier = Sequential(
+            Flatten(),
+            Linear(n, 4096),
+            Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+
+class ASiameseNetworks2(Module):
+    def __init__(self, input_shape):
+        """
+        :param input_shape: input image shape, (h, w, c)
+        """
+        super(SiameseNetworks, self).__init__()
+        self.net = Net(input_shape)
+
+        self.classifier = Sequential(
+            Linear(4096, 1, bias=False)            
+        )
+        # self._weight_init()
+
+    def _weight_init(self):
+        for m in self.modules():
+            if isinstance(m, Conv2d):
+                m.weight.data.normal_(0, 1e-2)
+                m.bias.data.normal_(0.5, 1e-2)
+            elif isinstance(m, Linear):
+                m.weight.data.normal_(0, 2.0 * 1e-1)
+                if m.bias is not None:
+                    m.bias.data.normal_(0.5, 1e-2)
+
+    def forward(self, x1, x2):
+        x1 = self.net(x1)
+        x2 = self.net(x2)
+        # L1 component-wise distance between vectors:
+        x = torch.pow(torch.abs(x1 - x2), 2.0)
+        return self.classifier(x)
+
+
+
+
+
+
+
+
+
+
+
+
