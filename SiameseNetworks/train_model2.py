c4dea4600714252855915ccde70d2596ef3a16fe
Alex Gain <doctorjuice1@gmail.com>
2020-01-21 23:50:44 -0800

    multi gpu support

--- SiameseNetworks/train_model2.py
+++ SiameseNetworks/train_model2.py
@@ -42,7 +42,7 @@ conf = {
     'nb_val_pairs': 10000,
     'nb_test_pairs': 10000,
 
-    'weight_decay': 1e-2,
+    'weight_decay': 1e-3,
     
     'lr_features': 0.00006,
     'lr_classifier': 0.00006,
