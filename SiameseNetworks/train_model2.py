6fbe79fe722abb6612ac7274fa7e587db846c5a3
Alex Gain <doctorjuice1@gmail.com>
2020-01-27 19:19:04 -0500

    Seq. Omniglot for classification

--- SiameseNetworks/train_model2.py
+++ SiameseNetworks/train_model2.py
@@ -79,7 +79,7 @@ for i in range(len(all_xy)):
     dataloaders.append([train_loader,test_loader])
 
 ## model and optimizer instantiations:
-net = Classifier(image_size = 105, output_shape=55, tasks=50)
+net = Classifier(image_size = 105, output_shape=60, tasks=50)
 if gpu_boole:
     net = net.cuda()
 optimizer = torch.optim.Adam(net.parameters(), lr = 1e-3)
