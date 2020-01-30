2e9999a434e0eb639e7a9787a33bebbf68c35edf
Alex Gain <doctorjuice1@gmail.com>
2020-01-30 01:55:59 -0500

    Seq. Omniglot for classification

--- SiameseNetworks/train_model2.py
+++ SiameseNetworks/train_model2.py
@@ -146,8 +146,8 @@ if gpu_boole:
     net = net.cuda()
 # optimizer = torch.optim.Adam(net.parameters(), lr = 1e-4)
 optimizer = torch.optim.Adam([
-                {'params': (param for name, param in net.named_parameters() if 'adjx' not in name), 'lr':1e-4,'momentum':0},
-                {'params': (param for name, param in net.named_parameters() if 'adjx' in name), 'lr':1e-4,'momentum':0,'weight_decay':args.decay}
+                {'params': (param for name, param in net.named_parameters() if 'adjx' not in name), 'lr':args.lr,'momentum':0},
+                {'params': (param for name, param in net.named_parameters() if 'adjx' in name), 'lr':args.lr,'momentum':0,'weight_decay':args.decay}
             ])
 
 ## train, test eval:
