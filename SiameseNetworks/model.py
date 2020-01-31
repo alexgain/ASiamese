89ac2c14a56cbfa35f1097664bd2454682d26270
Alex Gain <doctorjuice1@gmail.com>
2020-01-31 00:48:10 -0500

    Seq. Omniglot for classification

--- SiameseNetworks/model.py
+++ SiameseNetworks/model.py
@@ -649,9 +649,10 @@ def _adj_spars_loss(module, task, S=0):
         return S
 
 def _freeze_grads(module, task, hooks = []):
-    if any([isinstance(module, ALinear), isinstance(module, AConv2d)]):
-        if task > 0:
-            gradient_mask = ((module.adjx[0]) == 0.).data
+    if task == 0:
+        return []
+    if any([isinstance(module, ALinear), isinstance(module, AConv2d)]):        
+        gradient_mask = ((module.adjx[0]) == 0.).data
         for k in range(1, task):
             gradient_mask = gradient_mask * ((module.adjx[k]) == 0.).data
         gradient_mask = gradient_mask.float()
@@ -661,6 +662,7 @@ def _freeze_grads(module, task, hooks = []):
         for submodule in module.children():
             hooks = hooks + _freeze_grads(submodule, task, hooks)
         return hooks
+    
         
 
     
