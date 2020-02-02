ab1550ccdc49444a88e866021ba1d046d17e310a
Alex Gain <doctorjuice1@gmail.com>
2020-02-02 08:42:24 -0500

    Seq. Omniglot for classification

--- SiameseNetworks/model.py
+++ SiameseNetworks/model.py
@@ -191,10 +191,15 @@ class Classifier(nn.Module):
         :param image_input:
         :return:
         """
-        x = self.mp1(self.bn1(self.relu(self.conv1(image_input, dataset=task, round_ = round_))))
-        x = self.mp2(self.bn2(self.relu(self.conv2(x, dataset=task, round_ = round_))))
-        x = self.mp3(self.bn3(self.relu(self.conv3(x, dataset=task, round_ = round_))))
-        x = self.mp4(self.bn4(self.relu(self.conv4(x, dataset=task, round_ = round_))))
+        # x = self.mp1(self.bn1(self.relu(self.conv1(image_input, dataset=task, round_ = round_))))
+        # x = self.mp2(self.bn2(self.relu(self.conv2(x, dataset=task, round_ = round_))))
+        # x = self.mp3(self.bn3(self.relu(self.conv3(x, dataset=task, round_ = round_))))
+        # x = self.mp4(self.bn4(self.relu(self.conv4(x, dataset=task, round_ = round_))))
+
+        x = self.mp1(self.relu(self.conv1(image_input, dataset=task, round_ = round_)))
+        x = self.mp2(self.relu(self.conv2(x, dataset=task, round_ = round_)))
+        x = self.mp3(self.relu(self.conv3(x, dataset=task, round_ = round_)))
+        x = self.mp4(self.relu(self.conv4(x, dataset=task, round_ = round_)))
 
         x = x.view(x.size()[0], -1)
         x = self.linear(x, dataset=task, round_ = round_)
